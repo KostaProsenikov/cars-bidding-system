3a241f56643dabf29ae34282d30436d8234b5cce
Prosenikov <kostadin.prosenikov@gmail.com>
2025-03-14 18:55:29 +0200

    Add subscriptions and wallet entities

---
+++ src/main/java/app/wallet/model/Wallet.java
@@ -0,0 +1,44 @@
+package app.wallet.model;
+
+import app.user.model.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Currency;
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Wallet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne
+    private User owner;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private WalletStatus status;
+
+    private BigDecimal balance;
+
+    private Currency currency;
+
+    @Column(nullable = false)
+    private LocalDateTime createdOn;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedOn;
+
+
+}
