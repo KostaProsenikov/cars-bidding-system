664db5d39c4db4a6bbe991660d5d8fcc1220d1d1
Prosenikov <kostadin.prosenikov@gmail.com>
2025-03-19 18:47:43 +0200

    Add login, logout and register methods and handles

--- src/main/java/app/web/IndexController.java
+++ src/main/java/app/web/IndexController.java
@@ -1,5 +1,6 @@
 package app.web;
 
+import app.user.model.User;
 import app.web.dto.RegisterRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +10,8 @@ import app.web.dto.LoginRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import app.user.service.UserService;
 import org.springframework.web.bind.annotation.RequestParam;
+import app.user.service.UserService;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -25,12 +26,16 @@ public class IndexController {
     }
 
     @GetMapping("")
-    public String IndexController() {
-        return "index";
+    public ModelAndView getIndexPage() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index");
+        User user = new User();
+        modelAndView.addObject("user", user);
+        return modelAndView;
     }
 
     @GetMapping("/login")
-    public ModelAndView GetLoginPage(@RequestParam (value = "error", required = false) String errorParam) {
+    public ModelAndView GetLoginPage(@RequestParam(value = "error", required = false) String errorParam) {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("login");
         modelAndView.addObject("loginRequest", new LoginRequest());
@@ -52,13 +57,16 @@ public class IndexController {
 
     @PostMapping ("/register")
     public ModelAndView registerNewUser(@Valid RegisterRequest registerRequest, BindingResult bindingResult) {
-
         if (bindingResult.hasErrors()) {
             return new ModelAndView("register");
         }
 
-        userService.register(registerRequest);
+        User user = userService.register(registerRequest);
 
-        return new ModelAndView("redirect:/login");
+        if (user != null) {
+            return new ModelAndView("redirect:/login");
+        } else {
+            return new ModelAndView("register");
+        }
     }
 }
