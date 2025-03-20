df764245d3097b2411058174e53ff4df1cf80e1e
Kostadin Prosenikov <devmaster123@proton.me>
2025-03-20 10:11:05 +0200

    Add getting of the already created ads and displaying initial info on the home page.

--- src/main/java/app/advert/repository/AdvertRepository.java
+++ src/main/java/app/advert/repository/AdvertRepository.java
@@ -4,11 +4,14 @@ import app.advert.model.Advert;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface AdvertRepository extends JpaRepository<Advert, UUID> {
 
-    Optional<Advert> findAdvertByOwnerId(UUID ownerId);
+    List<Advert> findAdvertByOwnerId(UUID ownerId);
+
+    List<Advert> findByVisibleTrue();
 }
--- src/main/java/app/advert/service/AdvertService.java
+++ src/main/java/app/advert/service/AdvertService.java
@@ -8,6 +8,7 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class AdvertService {
@@ -49,5 +50,10 @@ public class AdvertService {
         System.out.printf("Advert created: %s%n", advert);
 
         return advert;
+    }
+
+    public List<Advert> getFirst20VisibleAdverts() {
+        List<Advert> adverts = advertRepository.findByVisibleTrue().stream().filter(a -> a.isVisible()).limit(20).toList();
+        return adverts;
     }
 }
