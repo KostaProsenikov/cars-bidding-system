821f2dae75f8ad559f0ddb4c93e379cbecb8a108
Kostadin Prosenikov <devmaster123@proton.me>
2025-03-23 17:39:31 +0200

    Add edit my profile form and updating of the user profile

--- src/main/java/app/web/UserController.java
+++ src/main/java/app/web/UserController.java
@@ -3,8 +3,14 @@ package app.web;
 import app.security.AuthenticationMetadata;
 import app.user.model.User;
 import app.user.service.UserService;
+import app.web.dto.UserEditRequest;
+import app.web.mapper.DtoMapper;
+import jakarta.validation.Valid;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -18,12 +24,27 @@ public class UserController {
         this.userService = userService;
     }
 
-    @RequestMapping ("/my-profile")
-    public ModelAndView getProfilePage(@AuthenticationPrincipal AuthenticationMetadata authenticationMetadata) {
+    @GetMapping ("/my-profile")
+    public ModelAndView getProfilePage(@AuthenticationPrincipal AuthenticationMetadata authenticationMetadata, UserEditRequest userEditRequest) {
         User user = userService.getById(authenticationMetadata.getUserId());
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.addObject("user", user);
+        modelAndView.addObject("userEditRequest", DtoMapper.mapUserToUserEditRequest(user));
         modelAndView.setViewName("my-profile");
         return modelAndView;
+    }
+
+    @PutMapping ("/{$userId}/profile")
+    public ModelAndView getProfilePage(@AuthenticationPrincipal AuthenticationMetadata authenticationMetadata, @Valid UserEditRequest userEditRequest, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            User user = userService.getById(authenticationMetadata.getUserId());
+            ModelAndView modelAndView = new ModelAndView();
+            modelAndView.addObject("user", user);
+            modelAndView.addObject("userEditRequest", userEditRequest);
+            modelAndView.setViewName("my-profile");
+            return modelAndView;
+        }
+        userService.updateUserDetails(authenticationMetadata.getUserId(), userEditRequest);
+        return new ModelAndView("redirect:/users/my-profile");
     }
 }
--- src/main/java/app/web/dto/UserEditRequest.java
+++ src/main/java/app/web/dto/UserEditRequest.java
@@ -2,18 +2,22 @@ package app.web.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.URL;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserEditRequest {
 
-    @Size(max = 20, message = "First name should be 20 characters max.")
+    @Size(min = 3, max = 20, message = "First name should be between 3 and 20 characters.")
     private String firstName;
 
-    @Size(max = 20, message = "Last name should be 20 characters max.")
+    @Size(min = 3, max = 20, message = "Last name should be between 3 and 20 characters.")
     private String lastName;
 
     @Email(message = "Email should be a valid email.")
