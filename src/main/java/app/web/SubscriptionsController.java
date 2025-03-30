513aadf9a551a0e2cfc5c9859fbc84084122758f
Prosenikov <k.prosenikov@blubito.com>
2025-03-30 14:52:49 +0300

    Add initial subscriptions dropdown selection and forms.

--- src/main/java/app/web/SubscriptionsController.java
+++ src/main/java/app/web/SubscriptionsController.java
@@ -1,24 +1,31 @@
 package app.web;
 
 import app.security.AuthenticationMetadata;
+import app.subscription.model.SubscriptionPeriod;
+import app.subscription.model.SubscriptionType;
+import app.subscription.service.SubscriptionService;
 import app.user.model.User;
 import app.user.service.UserService;
+import app.web.dto.UpgradeRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.UUID;
 
 @Controller
 @RequestMapping ("/subscriptions")
 public class SubscriptionsController {
 
     private final UserService userService;
+    private final SubscriptionService subscriptionService;
 
     @Autowired
-    public SubscriptionsController(UserService userService) {
+    public SubscriptionsController(UserService userService, SubscriptionService subscriptionService) {
         this.userService = userService;
+        this.subscriptionService = subscriptionService;
     }
 
     @GetMapping("")
@@ -28,5 +35,24 @@ public class SubscriptionsController {
         User user = userService.getById(authenticationMetadata.getUserId());
         modelAndView.addObject("user", user);
         return modelAndView;
+    }
+
+    @PostMapping("/change/{planType}")
+    public String changeSubscriptionType(@PathVariable int planType, @RequestParam("period") String period, @AuthenticationPrincipal AuthenticationMetadata authenticationMetadata) {
+        User user = userService.getById(authenticationMetadata.getUserId());
+        SubscriptionPeriod subscriptionPeriod = SubscriptionPeriod.valueOf(period.toUpperCase());
+        SubscriptionType subscriptionType = switch (planType) {
+            case 1 -> SubscriptionType.PLUS;
+            case 2 -> SubscriptionType.PROFESSIONAL;
+            default -> SubscriptionType.DEFAULT;
+        };
+        User subscriptionOwner = user;
+        UUID walletId =  subscriptionOwner.getWallets().getFirst().getId();
+        UpgradeRequest upgradeRequest = UpgradeRequest.builder()
+                .subscriptionPeriod(subscriptionPeriod)
+                .walletId(walletId)
+                .build();
+        subscriptionService.upgrade(user, subscriptionType, upgradeRequest);
+        return "redirect:/subscriptions";
     }
 }
